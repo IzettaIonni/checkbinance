8999ef0a5f4fdea588a40659c1f12da2b21f9008
Izetta <izettaionni@mail.ru>
2023-03-01 12:35:59 +0600

    Initial commit

---
+++ src/main/java/kz/insar/checkbinance/CheckbinanceApplication.java
@@ -0,0 +1,13 @@
+package kz.insar.checkbinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckbinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckbinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/insar/checkbinance/CheckbinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.insar.checkbinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckbinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

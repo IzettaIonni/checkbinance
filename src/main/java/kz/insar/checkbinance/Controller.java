715add6ac8213d669943de17655370d37fb49b57
Izetta <izettaionni@mail.ru>
2023-03-16 16:21:59 +0200

    1

--- src/main/java/kz/insar/checkbinance/Controller.java
+++ src/main/java/kz/insar/checkbinance/Controller.java
@@ -1,14 +1,19 @@
 package kz.insar.checkbinance;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("test")
+GET /test/hello
 public class Controller {
-    @GetMapping
+    @Autowired
+    private TextRepository repository;
+    @GetMapping("/hello")
     public String test() {
-        return "Hello World!";
+        return repository.get_hello();
     }
+
 }
---
+++ src/main/java/kz/insar/checkbinance/TextRepository.java
@@ -0,0 +1,11 @@
+package kz.insar.checkbinance;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TextRepository {
+
+    public String get_hello() {
+        return "Greeting";
+    }
+}

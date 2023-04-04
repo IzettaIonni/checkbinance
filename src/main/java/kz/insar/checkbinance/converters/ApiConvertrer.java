0c8eaee62d9a83283cd538f18499a44e9b07d2a2
Izetta <izettaionni@mail.ru>
2023-04-04 23:01:21 +0300

    1

--- src/main/java/kz/insar/checkbinance/converters/ApiConvertrer.java
+++ src/main/java/kz/insar/checkbinance/converters/ApiConvertrer.java
@@ -4,10 +4,18 @@ import kz.insar.checkbinance.api.LastPriceDTO;
 import kz.insar.checkbinance.client.RecentTradeDTO;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
 @Service
 public class ApiConvertrer {
 
     public LastPriceDTO toApi(RecentTradeDTO recentTrade) {
-        return null;
+        LastPriceDTO lastPriceDTO = new LastPriceDTO();
+        lastPriceDTO.setId(recentTrade.getId());
+        lastPriceDTO.setPrice(recentTrade.getPrice());
+        lastPriceDTO.setTime(LocalDateTime.ofInstant(Instant.ofEpochMilli(recentTrade.getTime()), TimeZone.getDefault().toZoneId()));
+        return lastPriceDTO;
     }
 }
